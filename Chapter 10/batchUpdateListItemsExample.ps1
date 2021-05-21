f78711918cbcbdf48459927deb82a91473ffd92e
Rene Modery <rene@modery.net>
2021-05-21 11:16:42 +0800

    PowerShell scripts to manage Microsoft Lists

---
+++ Chapter 10/batchUpdateListItemsExample.ps1
@@ -0,0 +1,19 @@
+﻿# Example: Updating all items in a list
+
+# Replace mytenant with your tenant's name, and the site URL, List name, and columns with your own
+
+Connect-PnPOnline -Url https://mytenant.sharepoint.com/sites/ITProjects -Credentials (Get-Credential) 
+
+$list = Get-PnPList -Identity "Planned IT Projects" 
+
+$items = Get-PnPListItem -List $list 
+
+foreach($item in $items) { 
+
+    Write-Host "Current Expected Cost: $($item["ExpectedCost"])" 
+
+    Write-Host "New Expected Cost: $($item["ExpectedCost"]*1.1)" 
+
+    Set-PnPListItem -List $list -Identity $item.Id -Values @{"ExpectedCost" = $item["ExpectedCost"]*1.1} 
+
+} 
